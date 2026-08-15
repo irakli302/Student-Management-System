c5564296aa6c329c0b3cb420c477de311de7f874
irakli302 <iraklidzandzava5@gmail.com>
2026-08-15 14:42:53 +0400

    Refactor Book and Student classes

--- src/main/java/com/example/student_managment/domain/Book.java
+++ src/main/java/com/example/student_managment/domain/Book.java
@@ -1,19 +1,25 @@
 package com.example.student_managment.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
+@Data
 @Table(name = "Books")
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String isbn;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
     private Student student;
 }
--- src/main/java/com/example/student_managment/domain/Student.java
+++ src/main/java/com/example/student_managment/domain/Student.java
@@ -1,4 +1,38 @@
 package com.example.student_managment.domain;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Students")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Book> books = new HashSet<>();
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "student_courses",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Course> courses = new HashSet<>();
 }
