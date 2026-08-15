43029a02d2583940df8eab0b0d8a4f7af61373c0
irakli302 <iraklidzandzava5@gmail.com>
2026-08-15 14:52:07 +0400

    Implemented Course class

--- src/main/java/com/example/student_managment/domain/Course.java
+++ src/main/java/com/example/student_managment/domain/Course.java
@@ -1,4 +1,30 @@
 package com.example.student_managment.domain;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Courses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String code;
+    private double credits;
+
+    @ManyToMany(mappedBy = "courses")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
+    private Set<Student>students = new HashSet<>();
 }
