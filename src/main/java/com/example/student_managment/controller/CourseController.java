64d7fb7cc2aea1ffff5fce027a805224a60a0775
irakli302 <iraklidzandzava5@gmail.com>
2026-08-25 20:25:51 +0400

    Implemented CourseController class

--- src/main/java/com/example/student_managment/controller/CourseController.java
+++ src/main/java/com/example/student_managment/controller/CourseController.java
@@ -1,4 +1,49 @@
 package com.example.student_managment.controller;
 
+import com.example.student_managment.domain.Course;
+import com.example.student_managment.domain.Student;
+import com.example.student_managment.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController {
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService){
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourse());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable long id) {
+        return ResponseEntity.ok(courseService.getCourseById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course createdCourse = courseService.createCourse(course);
+        return new ResponseEntity<>(createdCourse, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable long id) {
+        courseService.deleteCourse(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}/students")
+    public ResponseEntity<Set<Student>> getStudentsInCourse(@PathVariable long id) {
+        Set<Student> students = courseService.getStudentsInCourse(id);
+        return ResponseEntity.ok(students);
+    }
 }
